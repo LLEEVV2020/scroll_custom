6469f4c4c7056aa9cc92d125eeaaabbfd975be4d
LLEEVV2020 <lleevv2020@gmail.com>
2019-03-12 14:09:17 +0400

    add

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 (function(){
 //window.addEventListener('load', function(){
 
+/**
+ * Добавление обёртки для классов .table
+ */
+
 if ("ontouchstart" in document.documentElement) {
     mobile();
 }
